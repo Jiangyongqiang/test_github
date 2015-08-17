dd16fcad4310514e8530cced850832165651a401
Jiangyongqiang <jiangyongqiang2015@163.com>
2015-08-17 11:27:40 +0800

    upate

---
+++ first/src/com/test/test/activiy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.test.test.activiy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.test.test.R;
+
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		 
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main_activity);
+	}
+
+	
+ 
+}
