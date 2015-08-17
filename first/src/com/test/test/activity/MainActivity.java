c47cddaff3ed45dd1da93127221b9bf6432500fc
Jiangyongqiang <jiangyongqiang2015@163.com>
2015-08-17 16:32:33 +0800

    dd

--- first/src/com/test/test/activity/MainActivity.java
+++ first/src/com/test/test/activity/MainActivity.java
@@ -1,17 +1,24 @@
 package com.test.test.activity;
 
 import android.app.Activity;
+
 import android.os.Bundle;
 
 import com.test.test.R;
-
+/**
+ * 
+* @ClassName: MainActivity 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author android Jiangyongqiang jiangyongqiang2015@163.com
+* @date 2015年8月17日 下午12:53:01 
+*
+ */
 public class MainActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		 
 		super.onCreate(savedInstanceState);
-		
 		setContentView(R.layout.main_activity);
 	}
 
